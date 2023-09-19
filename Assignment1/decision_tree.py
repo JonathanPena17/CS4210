df1fe82a9ba3de73c9a61033110774a829225e0f
JonathanPena17 <jonathanrpena23@gmail.com>
2023-09-19 02:33:46 -0700

    Finished code (I think)

--- Assignment1/decision_tree.py
+++ Assignment1/decision_tree.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Jonathan Peña
+# FILENAME: decision_tree.py
+# SPECIFICATION: Complete the given python program (decision_tree.py) that will read the file contact_lens.csv and output a decision tree.
 # FOR: CS 4210- Assignment #1
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 4 hours
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
@@ -18,7 +18,7 @@ X = []
 Y = []
 
 #reading the data in a csv file
-with open('contact_lens.csv', 'r') as csvfile:
+with open('/Users/jonathanpena/Documents/CS4210/Assignment1/contact_lens.csv', 'r') as csvfile:
   reader = csv.reader(csvfile)
   for i, row in enumerate(reader):
       if i > 0: #skipping the header
@@ -30,9 +30,26 @@ with open('contact_lens.csv', 'r') as csvfile:
 #--> add your Python code here
 # X =
 
+for row in db:
+    age = 0
+    if row[0] == 'Young':
+        age = 1
+    elif row[0] == 'Prepresbyopic':
+        age = 2
+    elif row[0] == 'Presbyopic':
+        age = 3
+
+    spectacle = 1 if row[1] == 'Myope' else 2
+    astigmatism = 1 if row[2] == 'No' else 2
+    tear = 1 if row[3] == 'Reduced' else 2
+
+    X.append([age, spectacle, astigmatism, tear])
+
 #transform the original categorical training classes into numbers and add to the vector Y. For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
 #--> addd your Python code here
 # Y =
+for row in db:
+    Y.append(1 if row[4] == 'No' else 2)
 
 #fitting the decision tree to the data
 clf = tree.DecisionTreeClassifier(criterion = 'entropy')
