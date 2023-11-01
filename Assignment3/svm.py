090ff664aaf884cad87bea90a5dbdda6a1d0b7a7
JonathanPena17 <jonathanrpena23@gmail.com>
2023-11-01 12:53:17 -0700

    finished code

--- Assignment3/svm.py
+++ Assignment3/svm.py
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
+# AUTHOR: Jonathan Peña
+# FILENAME: sym.py
 # SPECIFICATION: description of the program
 # FOR: CS 4210- Assignment #3
 # TIME SPENT: how long it took you to complete the assignment
@@ -19,41 +19,48 @@ degree = [1, 2, 3]
 kernel = ["linear", "poly", "rbf"]
 decision_function_shape = ["ovo", "ovr"]
 
-df = pd.read_csv('optdigits.tra', sep=',', header=None) #reading the training data by using Pandas library
+df = pd.read_csv('Assignment3/optdigits.tra', sep=',', header=None) #reading the training data by using Pandas library
 
 X_training = np.array(df.values)[:,:64] #getting the first 64 fields to create the feature training data and convert them to NumPy array
 y_training = np.array(df.values)[:,-1] #getting the last field to create the class training data and convert them to NumPy array
 
-df = pd.read_csv('optdigits.tes', sep=',', header=None) #reading the training data by using Pandas library
+df = pd.read_csv('Assignment3/optdigits.tes', sep=',', header=None) #reading the training data by using Pandas library
 
 X_test = np.array(df.values)[:,:64] #getting the first 64 fields to create the feature testing data and convert them to NumPy array
 y_test = np.array(df.values)[:,-1] #getting the last field to create the class testing data and convert them to NumPy array
 
 #created 4 nested for loops that will iterate through the values of c, degree, kernel, and decision_function_shape
-#--> add your Python code here
+highest_accuracy = 0
 
-for :
-    for :
-        for :
-           for :
+for i in c:
+    for j in degree:
+        for k in kernel:
+           for l in decision_function_shape:
 
                 #Create an SVM classifier that will test all combinations of c, degree, kernel, and decision_function_shape.
                 #For instance svm.SVC(c=1, degree=1, kernel="linear", decision_function_shape = "ovo")
-                #--> add your Python code here
+                clf = svm.SVC(C=i, degree=j, kernel=k, decision_function_shape=l)
 
                 #Fit SVM to the training data
-                #--> add your Python code here
+                clf.fit(X_training, y_training)
 
                 #make the SVM prediction for each test sample and start computing its accuracy
                 #hint: to iterate over two collections simultaneously, use zip()
                 #Example. for (x_testSample, y_testSample) in zip(X_test, y_test):
                 #to make a prediction do: clf.predict([x_testSample])
-                #--> add your Python code here
+                correct_predictions = 0
+
+                for (x_testSample, y_testSample) in zip(X_test, y_test):
+                    prediction = clf.predict([x_testSample])[0]
+                    if prediction == y_testSample:
+                        correct_predictions += 1
 
                 #check if the calculated accuracy is higher than the previously one calculated. If so, update the highest accuracy and print it together
                 #with the SVM hyperparameters. Example: "Highest SVM accuracy so far: 0.92, Parameters: a=1, degree=2, kernel= poly, decision_function_shape = 'ovo'"
-                #--> add your Python code here
+                accuracy = correct_predictions / len(y_test)
 
+                if accuracy > highest_accuracy:
+                    highest_accuracy = accuracy
+                    print(f"Highest SVM accuracy so far: {highest_accuracy:.2f}, Parameters: C={i}, degree={j}, kernel={k}, decision_function_shape={l}")
 
-
-
+print(f"Final highest SVM accuracy: {highest_accuracy:.2f}")
